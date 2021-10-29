dbbb52455b84102412773d924ebddc2d4f39b937
Fabián Karaben <fabiankaraben@gmail.com>
2021-10-29 14:23:10 -0300

    Empty project.

---
+++ public/component---src-pages-404-js-488d2bee5b9006fa868c.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkbackend_verse_website=self.webpackChunkbackend_verse_website||[]).push([[883],{9616:function(e,t,a){a.r(t);var n=a(7294),s=a(7198),l=a(3751);t.default=function(e){var t=e.data,a=e.location,u=t.site.siteMetadata.title;return n.createElement(s.Z,{location:a,title:u},n.createElement(l.Z,{title:"404: Not Found"}),n.createElement("h1",null,"404: Not Found"),n.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-488d2bee5b9006fa868c.js.map
